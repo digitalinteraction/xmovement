ab5a43fec42af2f7024c15c81cf2a0a70f110cd6
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-02-10 15:01:06 +0000

    Added Event factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,7 +15,21 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
+        'userName' => $faker->userName,
+        'avatar' => $faker->imageUrl($width = 960, $height = 960, 'people'),
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+        'created_at' => $faker->dateTimeBetween($startDate = '-10 days', $endDate = '-6 days'),
+        'updated_at' => $faker->dateTimeBetween($startDate = '-6 days', $endDate = '-4 days'),
+    ];
+});
+
+$factory->define(App\Event::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'description' => $faker->text($maxNbChars = 400),
+        'photo' => $faker->imageUrl($width = 1200, $height = 400),
+        'created_at' => $faker->dateTimeBetween($startDate = '-4 days', $endDate = '-2 days'),
+        'updated_at' => $faker->dateTimeBetween($startDate = '-2 days', $endDate = 'now'),
     ];
 });
