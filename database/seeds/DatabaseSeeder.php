68e1421c381e62fd75af5c32d14172b858101167
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-02-10 11:10:23 +0000

    Added User table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use Illuminate\Database\Eloquent\Model;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+	    Model::unguard();
+
+	    $this->call(UsersTableSeeder::class);
+	    
+	    Model::reguard();
     }
 }
