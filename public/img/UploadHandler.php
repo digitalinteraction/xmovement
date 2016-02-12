5ec250f53081d31a33db16d9be2f5ce81561d156
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-02-12 14:08:52 +0000

    Changed dir in upload handler

--- public/img/UploadHandler.php
+++ public/img/UploadHandler.php
@@ -45,8 +45,8 @@ class UploadHandler
     function __construct($options = null, $initialize = true, $error_messages = null) {
         $this->options = array(
             'script_url' => $this->get_full_url().'/',
-            'upload_dir' => dirname($this->get_server_var('SCRIPT_FILENAME')).'/ideas/',
-            'upload_url' => $this->get_full_url().'/ideas/',
+            'upload_dir' => dirname($this->get_server_var('SCRIPT_FILENAME')).'/uploads/',
+            'upload_url' => $this->get_full_url().'/uploads/',
             'user_dirs' => false,
             'mkdir_mode' => 0755,
             'param_name' => 'files',
